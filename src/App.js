ac821a1fc665cf699222341818c7fe9c16b9f819
thirumurugan7 <thiru82003@gmail.com>
2023-03-17 22:56:43 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import "./App.css";
 import InputComponent from "./components/InputComponent";
 function App() {
   return (
-    <div className=" bg-gradient-to-r from-cyan-500 to-blue-500">
-      <div className="pt-14 px-14">
-        <div className="text-bold">
-          <h2 className="text-white text-lg font-bold">Hi 👋</h2>
-          <h2 className="text-white text-xl mt-6">
-            Welcome to my first project
-          </h2>
-        </div>
-        <div className="mt-6 ">
-          <div>
-            <InputComponent />
+    <>
+      <div className=" bg-gradient-to-r from-cyan-500 to-blue-500 min-h-screen flex justify-start items-start">
+        <div className="pt-14 px-14 mt-20">
+          <div className="text-bold ml-10">
+            <h2 className="text-white text-[40px] font-bold">Hi 👋</h2>
+            <h2 className="text-white text-xl mt-6">
+              Welcome to my first project
+            </h2>
+          </div>
+          <div className="mt-10 ml-10 ">
+            <div>
+              <InputComponent />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className=" bg-gradient-to-r from-cyan-500 to-blue-500  text-white flex justify-center items-center">
+        <h3 className="text-lg  fixed bottom-0">
+          {" "}
+          Made with ❤ by Gryffindors{" "}
+        </h3>
+      </div>
+    </>
   );
 }
 
--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -4,23 +4,19 @@ const InputComponent = () => {
   return (
     <div>
       <label className="font-rajdhani font-semibold text-2xl text-white">
-        Name
+        Folks we need your name to Proceed
       </label>
       <input
         type="text"
         placeholder="Enter your name to start"
-        className=" mx-5 bg-siteDimBlack text-black outline-none focus:outline-siteViolet p-2 rounded-md sm:max-w-[40%] max-w-full"
+        className="block mt-5 bg-siteDimBlack text-black outline-none focus:outline-siteViolet p-2 rounded-md sm:max-w-[80%] max-w-full"
       />
 
-      <div className="mt-4 text-center px-15 ml-20">
-        <button className="block border border-pink-200 px-4 rounded-full py-1 text-md font-semibold hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+      <div className="mt-4 text-center px-25 ml-7">
+        <button className="block border border-pink-200 px-10 rounded-full py-1 text-[20px] font-semibold hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
           {" "}
           submit
         </button>
-      </div>
-
-      <div className="bottom-0 text-yellow-200">
-        Made with Love by Gryffindors
       </div>
     </div>
   );
